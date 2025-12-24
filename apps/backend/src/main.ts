b8ff96facb13191643771e461dc1247f9345bd6c
anchalita.ka <61132765+anchalitaka@users.noreply.github.com>
2025-12-24 18:21:30 +0700

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3002;
   await app.listen(port);
 
   console.log(`🚀 Backend is running on: http://localhost:${port}/api`);
